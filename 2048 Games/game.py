2a606ed34acdc2caf9304cc0354e9928aa08654f
Susmita Dey <susmitadey475@gmail.com>
2025-02-03 16:28:09 +0530

    Create game.py

---
+++ 2048 Games/game.py
@@ -0,0 +1,151 @@
+# 2048 Game
+import tkinter as tk
+import random
+
+
+class Game(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        self.grid()
+        # give title to the game area
+        self.master.title("2048")
+        # set border colors and height and width of grid and tiles
+        self.main_grid = tk.Frame(
+            self,
+            bg="#92877d",
+            bd=5,
+            width=800,
+            height=800
+        )
+        # padding for score area
+        self.main_grid.grid(pady=(160, 0))
+
+        # create GUI
+        self.make_GUI()
+        # start game function
+
+        # # key bindings
+        # self.master.bind("<Left>", self.left)
+        # self.master.bind("<Right>", self.right)
+        # self.master.bind("<Up>", self.up)
+        # self.master.bind("<Down>", self.down)
+
+        # loops to keep the board running
+        self.mainloop()
+
+    Color_EmptyCell = "#ffd5b5"
+    Font_ScoreLabel = ("Verdana", 24)
+    Font_Score = ("Helvetica", 48, "bold")
+    Font_GameOver = ("Helvetica", 48, "bold")
+    Font_Color_GameOver = "#ffffff"
+    Winner_BG = "#ffcc00"
+    Color_Background = "#a39489"
+    Color_FG = "#ffffff"
+
+    Color_Cells = {
+        2: "#fcefe6",
+        4: "#f2e8cb",
+        8: "#f5b682",
+        16: "#f29446",
+        32: "#ff775c",
+        64: "#e64c2e",
+        128: "#ede291",
+        256: "#fce130",
+        512: "#ffdb4a",
+        1024: "#f0b922",
+        2048: "#fad74d"
+    }
+
+    Color_CellNumber = {
+        2: "#695c57",
+        4: "#695c57",
+        8: "#ffffff",
+        16: "#ffffff",
+        32: "#ffffff",
+        64: "#ffffff",
+        128: "#ffffff",
+        256: "#ffffff",
+        512: "#ffffff",
+        1024: "#ffffff",
+        2048: "#ffffff"
+    }
+
+    Fonts_CellNumber = {
+        2: ("Helvetica", 55, "bold"),
+        4: ("Helvetica", 55, "bold"),
+        8: ("Helvetica", 55, "bold"),
+        16: ("Helvetica", 50, "bold"),
+        32: ("Helvetica", 50, "bold"),
+        64: ("Helvetica", 50, "bold"),
+        128: ("Helvetica", 45, "bold"),
+        256: ("Helvetica", 45, "bold"),
+        512: ("Helvetica", 45, "bold"),
+        1024: ("Helvetica", 40, "bold"),
+        2048: ("Helvetica", 40, "bold"),
+    }
+
+    def make_GUI(self):
+        self.cells = []
+        for i in range(4):
+            row = []
+            for j in range(4):
+                cell_frame = tk.Frame(
+                    self.main_grid,
+                    bg=self.Color_EmptyCell,
+                    width=150,
+                    height=150
+                )
+                cell_frame.grid(row=i, column=j, padx=5, pady=5)
+                cell_number = tk.Label(self.main_grid, bg=self.Color_EmptyCell)
+                cell_number.grid(row=i, column=j)
+                cell_data = {"frame": cell_frame, "number": cell_number}
+                row.append(cell_data)
+            self.cells.append(row)
+
+        score_frame = tk.Frame()
+        score_frame.place(relx=0.5, y=65, anchor="center")
+        tk.Label(
+            score_frame,
+            text="Score",
+            font=self.Font_ScoreLabel
+        ).grid(row=0)
+        self.score_label = tk.Label(
+            score_frame,
+            text="0",
+            font=self.Font_Score
+        )
+        self.score_label.grid(row=1)
+
+    def start_game(self):
+        self.matrix = [[0] * 4 for _ in range(4)]
+        row = random.randint(0, 3)
+        col = random.randint(0, 3)
+        self.matrix[row][col] = 2
+        self.cells[row][col]["frame"].configure(bg=self.Color_Cells[2])
+        self.cells[row][col]["number"].configure(
+            bg=self.Color_Cells[2],
+            fg=self.Color_CellNumber[2],
+            font=self.Fonts_CellNumber[2],
+            text="2"
+        )
+        while (self.matrix[row][col] != 0):
+            row = random.randint(0, 3)
+            col = random.randint(0, 3)
+        self.matrix[row][col] = 2
+        self.cells[row][col]["frame"].configure(bg=self.Color_Cells[2])
+        self.cells[row][col]["number"].configure(
+            bg=self.Color_Cells[2],
+            fg=self.Color_CellNumber[2],
+            font=self.Fonts_CellNumber[2],
+            text="2"
+        )
+        self.score = 0
+
+
+def main():
+    print('hello')
+    Game()
+
+
+if __name__ == "__main__":
+    main()
